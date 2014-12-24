aef552b9879cd8d022224eb6205782e14262d1f0
Chris Hitchcott <hitchcott@gmail.com>
2014-12-24 22:29:21 +0800

    Add some assets, update some text

---
+++ readme.md
@@ -0,0 +1,5 @@
+# After Effects + Extend Script Experiment
+
+1. Install After Effects CS 2014
+2. Run `start.sh`
+3. Peruse `/output/`
--- start.sh
+++ start.sh
@@ -2,15 +2,18 @@
 
 if ! (ps aux | grep "After Effects -noui" | grep -v 'grep');
   then
-    echo "AfterEffects isn't running. Starting in background."
+    echo "After Effects isn't running. Starting in background."
     echo "Please wait for 10 seconds..."
     open -n -a "Adobe After Effects CC 2014.app" --args -noui;
     sleep 10;
-    echo "Please cancel and retry again if it hasn't started yet..."
-    echo "Ensure you have After Effect installed, and 'start.sh' is configured correctly."
+    echo "";
+    echo "Assuming you have After Effects installed and 'start.sh' is configured correctly,"
+    echo "After Effects will now remain open until you manually close it."
 fi
 
-echo 'Starting script... Check /output/ for result.'
+echo "";
+echo "Starting script...";
+echo "After Effects should play a chime once the render is compelte. Please Check /output/ for result.";
 
 # convoluted way of running jsx in no ui mode
 osascript src/osx_start.scpt
