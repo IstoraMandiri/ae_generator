9127e2777257c81da29ba917ed5a12b67d2e3436
Chris Hitchcott <hitchcott@gmail.com>
2014-12-24 22:14:38 +0800

    Better startup script

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-AfterEffects='Adobe After Effects CC 2014.app'
-number=$(ps aux | grep '$AfterEffects' | grep -v grep | wc -l)
-if [ $number -gt 0 ]
-    then
-        echo 'Already Running AE';
-    else
-        echo 'Starting AE in noUI mode';
-        # open after effects in NO UI mode
-        open -n -a "$AfterEffects" --args -noui
+
+if ! (ps aux | grep "After Effects -noui" | grep -v 'grep');
+  then
+    echo "AfterEffects isn't running. Starting in background."
+    echo "Please wait for 10 seconds..."
+    open -n -a "Adobe After Effects CC 2014.app" --args -noui;
+    sleep 10;
+    echo "Please cancel and retry again if it hasn't started yet..."
+    echo "Ensure you have After Effect installed, and 'start.sh' is configured correctly."
 fi
+
+echo 'Starting script... Check /output/ for result.'
 
 # convoluted way of running jsx in no ui mode
 osascript src/osx_start.scpt
